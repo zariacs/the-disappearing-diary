1c0c88e352aee7cf5efc88981d8d1c8378ffe206
Zaria Chen Shui <zariachenshui@gmail.com>
2023-06-14 23:14:43 -0500

    Updated contact info

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -3,8 +3,13 @@
 </script>
 
 <template>
-  <div class="heading-wrapper">
-    <h1>MEET THE DEVELOPER</h1>
+  <div class="container">
+    <a href="https://www.linkedin.com/in/zaria-chen-shui/" target="_blank">
+      <img class="linkedin-profile" src="../assets/images/linkedin-profile.png" alt="" >
+    <h2>Meet the Developer</h2>
+
+    </a>
+
   </div>
 </template>
 
@@ -12,9 +17,39 @@
 
 @use '@/assets/styles/scss/main';
 
-div {
-    @include main.route-placeholder();
-}
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
 
+  h2 {
+    color: main.$mauve;
+    font-weight: 900;
+    padding-top: 4.5rem;
+    font-size: 2rem;
+    letter-spacing: 0.3rem;
+    text-transform: uppercase;
+    text-align: center;
+  }
+
+  a {
+    text-decoration:none;
+  }
+
+  .linkedin-profile {
+    border-radius: 10px;
+    cursor: pointer;
+    box-shadow: 0 4px 40px rgba(0, 0, 0, 0.3);
+    transition: 0.2s ease-in;
+
+
+    &:hover {
+      box-shadow: 0 4px 50px rgba(255, 0, 0, 0.3);
+      transition: 0.2s ease-in;
+    }
+  }
+}
 
 </style>
